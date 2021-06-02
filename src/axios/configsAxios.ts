6cde7c92634b67c937589cc089f26aa5d4b313e8
artigeministars <@1Github@>
2021-06-02 20:56:56 +0300

    minimized+

--- src/axios/configsAxios.ts
+++
@@ -1,8 +0,0 @@
-export const configBase: IAxiosConfig = {
-    baseURL: "https://yoursite.com/api",
-    responseType: "json",
-    headers: {
-        "Content-Type": "application/json"
-    }
-};
-// transformRequest , transformResponse for type transformation
